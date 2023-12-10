735c2f8ace09849f0cc0f732a525b894d7ac2e32
Domasen <domas.nemanius@gmail.com>
2023-12-11 01:16:06 +0200

    Added:   POST Staff   GET Staff/Staffs   GET Staff/{id}

--- API/API/UsersComponent/Controllers/StaffController.cs
+++ API/API/UsersComponent/Controllers/StaffController.cs
@@ -1,5 +1,7 @@
-﻿using API.UsersComponent.Models;
+﻿using API.Data;
+using API.UsersComponent.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.UsersComponent.Controllers;
 
@@ -8,30 +10,38 @@ namespace API.UsersComponent.Controllers;
 public class StaffController : ControllerBase
 {
     private readonly ILogger<StaffController> _logger;
-
-    public StaffController(ILogger<StaffController> logger)
+    private readonly DataContext _context;
+    public StaffController(ILogger<StaffController> logger, DataContext context)
     {
         _logger = logger;
+        _context = context;
     }
     
     [HttpPost("Staff")]
     [ProducesResponseType(StatusCodes.Status201Created)]
-    public ActionResult<Staff> CreateStaff([FromBody] Staff createStaffDto)
+    public async Task<ActionResult<Staff>> CreateStaff(Staff staff)
     {
+        staff.Id = Guid.NewGuid();
+        _context.Staffs.Add(staff);
+        await _context.SaveChangesAsync();
 
-        var staff = new Staff
-        {
-            Id = Guid.NewGuid(),
-            FirstName = createStaffDto.FirstName,
-            LastName = createStaffDto.LastName,
-            PhoneNumber = createStaffDto.PhoneNumber,
-            Email = createStaffDto.Email,
-            HireDate = createStaffDto.HireDate
-            
-        };
-
-        return Ok(staff);
+        return Ok(await _context.Staffs.FindAsync(staff.Id));
     } 
     
+    [HttpGet("Staff/Staffs")]
+    public async Task<ActionResult<List<Staff>>> GetStaffs()
+    {
+        return Ok(await _context.Staffs.ToListAsync());
+    }
     
+    [HttpGet("Staff/{id}")]
+    public async Task<ActionResult<Staff>> GetStaff(Guid id)
+    {
+        var staff = await _context.Staffs.FindAsync(id);
+        if (staff == null)
+        {
+            return BadRequest("Staff not found.");
+        }
+        return Ok(staff);
+    } 
 }
