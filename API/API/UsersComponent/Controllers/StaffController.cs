9995403570f0e0796585006f91242bb8a4bc6968
Domasen <domas.nemanius@gmail.com>
2023-12-10 23:55:17 +0200

    Added Users Component folders and created Staff.cs and StaffController.cs

---
+++ API/API/UsersComponent/Controllers/StaffController.cs
@@ -0,0 +1,37 @@
+﻿using API.UsersComponent.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.UsersComponent.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class StaffController : ControllerBase
+{
+    private readonly ILogger<StaffController> _logger;
+
+    public StaffController(ILogger<StaffController> logger)
+    {
+        _logger = logger;
+    }
+    
+    [HttpPost("Staff")]
+    [ProducesResponseType(StatusCodes.Status201Created)]
+    public ActionResult<Staff> CreateStaff([FromBody] Staff createStaffDto)
+    {
+
+        var staff = new Staff
+        {
+            Id = Guid.NewGuid(),
+            FirstName = createStaffDto.FirstName,
+            LastName = createStaffDto.LastName,
+            PhoneNumber = createStaffDto.PhoneNumber,
+            Email = createStaffDto.Email,
+            HireDate = createStaffDto.HireDate
+            
+        };
+
+        return Ok(staff);
+    } 
+    
+    
+}
---
+++ API/API/UsersComponent/Models/Staff.cs
@@ -0,0 +1,12 @@
+﻿namespace API.UsersComponent.Models;
+
+public class Staff
+{
+    public Guid Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string PhoneNumber { get; set; }
+    public string Email { get; set; }
+    public DateTime HireDate { get; set; }
+
+}
