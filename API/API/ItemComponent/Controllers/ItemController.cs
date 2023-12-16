5e7b885362f56fb78016b590fd4ea6cd7c5ce57f
Samusiovaite <kamilesamusiovaite@gmail.com>
2023-12-16 21:40:52 +0200

    Create: Item Component (Repository, Controller, IItemRepository)

---
+++ API/API/ItemComponent/Controllers/ItemController.cs
@@ -0,0 +1,126 @@
+﻿using API.Data;
+using API.ItemComponent.Models;
+using API.ItemComponent.Repository;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.ItemComponent.Controllers;
+// https://www.pragimtech.com/blog/blazor/delete-in-asp.net-core-rest-api/
+[ApiController]
+[Route("[controller]")]
+
+public class ItemController : ControllerBase
+{
+    private readonly ILogger<ItemController> _logger;
+    private readonly IItemRepository _itemRepository;
+    
+    public ItemController(ILogger<ItemController> logger, IItemRepository itemRepository)
+    {
+        _logger = logger;
+        _itemRepository = itemRepository;
+    }
+    [HttpPost("Item")]
+    [ProducesResponseType(StatusCodes.Status201Created)]
+    
+    public async Task<ActionResult<Item>> CreateItem(Item? item)
+    {
+        try
+        {
+            if (item == null)
+            {
+                return BadRequest();
+            }
+
+            var createdItem = await _itemRepository.AddItem(item);
+
+            return CreatedAtAction(nameof(GetItems), new { id = createdItem.id }, createdItem);
+        }
+        catch (Exception e)
+        {
+            return StatusCode(StatusCodes.Status500InternalServerError, "Error retrieving data from the database");
+        }
+    }
+    
+    [HttpGet("Item/Items")]
+    public async Task<ActionResult<List<Item>>> GetItems()
+    {
+        try
+        {
+            return Ok(await _itemRepository.GetItem());
+        }
+        catch (Exception)
+        {
+            return StatusCode(StatusCodes.Status500InternalServerError,
+                "Error retrieving data from the database");
+        }
+    }
+    
+    [HttpGet("Item/{id}")]
+    public async Task<ActionResult<Item>> GetItem(Guid id)
+    {
+        try
+        {
+            var result = await _itemRepository.GetItem(id);
+
+            if (result == null)
+            {
+                return NotFound();
+            }
+
+            return result;
+        }
+        catch (Exception)
+        {
+            return StatusCode(StatusCodes.Status500InternalServerError,
+                "Error retrieving data from the database");
+        }
+    }
+    
+    [HttpDelete("Item/{id}")]
+    public async Task<ActionResult<Item>> DeleteItem(Guid id)
+    {
+        try
+        {
+            var itemToDelete = await _itemRepository.GetItem(id);
+
+            if (itemToDelete == null)
+            {
+                return NotFound($"Item with Id = {id} not found");
+            }
+
+            return await _itemRepository.DeleteItem(id);
+        }
+        catch (Exception)
+        {
+            return StatusCode(StatusCodes.Status500InternalServerError,
+                "Error deleting data");
+        }
+    }
+    
+    [HttpPut("Item/{id}")]
+    public async Task<ActionResult<Item>> UpdateItem(Guid id, Item item)
+    {
+        try
+        {
+            if(id != item.id)
+            {
+                return BadRequest("Item ID mismatch");
+            }
+
+            var itemToUpdate = await _itemRepository.GetItem(id);
+
+            if(itemToUpdate == null)
+            {
+                return NotFound($"Item with Id = {id} not found");
+            }
+
+            return await _itemRepository.UpdateItem(item);
+        }
+        catch (Exception)
+        {
+            return StatusCode(StatusCodes.Status500InternalServerError,
+                "Error updating data");
+        }
+    }
+ 
+}
---
+++ API/API/ItemComponent/Repository/ItemRepository.cs
@@ -0,0 +1,72 @@
+﻿using API.Data;
+using API.ItemComponent.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.ItemComponent.Repository;
+
+public class ItemRepository : IItemRepository
+{
+    private readonly DataContext _context;
+    
+    public ItemRepository(DataContext context)
+    {
+        _context = context;
+    }
+    
+    public async Task<Item> AddItem(Item item)
+    {
+        item.id = Guid.NewGuid();
+        var result = await _context.Item.AddAsync(item);
+        await _context.SaveChangesAsync();
+        return result.Entity;
+    }
+    
+    public async Task<Item?> DeleteItem(Guid itemId)
+    {
+        var result = await _context.Item.FirstOrDefaultAsync(s => s.id == itemId);
+
+        if (result != null)
+        {
+            _context.Item.Remove(result);
+            await _context.SaveChangesAsync();
+            return result;
+        }
+
+        return null;
+    }
+    
+    public async Task<Item?> GetItem(Guid itemId)
+    {
+        return await _context.Item.FirstOrDefaultAsync(s => s.id == itemId);
+    }
+
+    public async Task<IEnumerable<Item>> GetItem()
+    {
+        return await _context.Item.ToListAsync();
+    }
+    
+    
+    
+    public async Task<Item?> UpdateItem(Item item)
+    {
+        var result = await _context.Item.FirstOrDefaultAsync(s => s.id == item.id);
+
+        if (result != null)
+        {
+            result.id = item.id;
+            result.title = item.title;
+            result.imageURL = item.imageURL;
+            result.description = item.description;
+            result.amountInStock = item.amountInStock;
+            result.discountPercentage = item.discountPercentage;
+            result.price = item.price;
+            result.isHidden = item.isHidden;
+
+            await _context.SaveChangesAsync();
+
+            return result;
+        }
+
+        return null;
+    }
+}
