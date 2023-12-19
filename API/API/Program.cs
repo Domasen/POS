d4b7f687b8f9189ccb378f2ecb0e9be3df0254c2
Domasen <domas.nemanius@gmail.com>
2023-12-19 22:39:24 +0200

    Added CustomerStaffController.cs

--- API/API/Program.cs
+++ API/API/Program.cs
@@ -17,6 +17,7 @@ builder.Services.AddDbContext<DataContext>(options => options.UseSqlite(builder.
 builder.Services.AddScoped<IStaffRepository, StaffRepository>();
 builder.Services.AddScoped<IItemRepository, ItemRepository>();
 builder.Services.AddScoped<IServiceRepository, ServiceRepository>();
+builder.Services.AddScoped<ICustomerRepository, CustomerRepository>();
 
 var app = builder.Build();
 
