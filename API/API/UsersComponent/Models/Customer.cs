6824652f5cecdff0f52ec37ee2e35cf0ceda4a06
Domasen <domas.nemanius@gmail.com>
2023-12-19 21:32:28 +0200

    Created Customer.cs model

---
+++ API/API/UsersComponent/Models/Customer.cs
@@ -0,0 +1,13 @@
+﻿namespace API.UsersComponent.Models;
+
+public class Customer
+{
+    public Guid Id { get; set; }
+    public Guid DiscountId { get; set; }
+    public string Name { get; set; } = String.Empty;
+    public string LastName { get; set; } = String.Empty;
+    public DateTime Birthday { get; set; }
+    public string Address { get; set; } = String.Empty;
+    public int Points { get; set; }
+
+}
