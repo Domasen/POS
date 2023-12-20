0d14bd15702bc1d7a905d04142669f091ddd8f74
Domasen <domas.nemanius@gmail.com>
2023-12-20 22:39:41 +0200

    Added Order.cs

---
+++ API/API/OrdersComponent/Models/Order.cs
@@ -0,0 +1,18 @@
+﻿namespace API.OrdersComponent.Models;
+
+public enum Status
+{
+    Paid,
+    Unpaid
+}
+
+public class Order
+{
+    public Guid Id { get; set; }
+    public Guid CustomerId { get; set; }
+    public Guid StaffId { get; set; }
+    public Status Status{ get; set; }
+    public DateTime Date { get; set; }
+    public Decimal TotalAmount { get; set; }
+    public Decimal Tip { get; set; }
+}
