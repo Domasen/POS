b62747ee5c62d1884f59c47faefc1c9f0f41b120
Domasen <domas.nemanius@gmail.com>
2023-12-21 00:33:43 +0200

    Created OrderItem.cs

---
+++ API/API/OrdersComponent/Models/OrderItem.cs
@@ -0,0 +1,17 @@
+﻿namespace API.OrdersComponent.Models;
+
+public enum OrderItemType
+{
+    
+}
+
+public class OrderItem
+{
+    public Guid Id { get; set; }
+    public Guid OrderId { get; set; }
+    public Guid ItemId { get; set; }
+    public Decimal UnitPrice { get; set; }
+    public OrderItemType Type { get; set; }
+    public Decimal Quantity { get; set; }
+    public Decimal Subtotal { get; set; }
+}
